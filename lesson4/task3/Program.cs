268d009561cf6147d6257a24cb684f147fe375b9
Tatyana Sidorova <tasidorowa@gmail.com>
2024-03-04 20:15:56 +0300

    Добавили задание 3

---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿string GetLetters (string s)
+{
+string letters ="";
+foreach(char e in s)
+{
+    if (char.IsAsciiLetter(e)==true) letters+=e;
+}
+return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.Write(result);
