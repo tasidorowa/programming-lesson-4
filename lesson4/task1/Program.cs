e37aa2955a4376a58ffab5b3206b360465ae1d50
Tatyana Sidorova <tasidorowa@gmail.com>
2024-03-04 20:15:05 +0300

    Добавили задание 1

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,44 @@
+﻿// int[,] matrix = new int[4, 5];
+
+// Random rnd = new Random();
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = rnd.Next(1, 11);
+//         Console.Write($"{matrix[i, j]} "); //интерполяция строк
+//     }
+//     Console.WriteLine();
+// }
+
+int[,] CreateMatrix(int rowsCount, int columsCount)
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} "); //интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
